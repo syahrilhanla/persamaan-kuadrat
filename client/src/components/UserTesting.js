3950b8c22313f3b8bf4d1f251f5bb2e79d0527db
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-02-15 14:55:31 +0800

    got the questions with D below 0

--- client/src/components/UserTesting.js
+++ client/src/components/UserTesting.js
@@ -1,14 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GlobalContext } from './GlobalContext';
 
 export const UserTesting = () => {
   const { valuesOverZero, questionsData } = useContext(GlobalContext);
+  // console.log(valuesOverZero[0])
 
-  const questionsID = questionsData.map(item => valuesOverZero[0].filter(questions => questions.id === item.id));
-  console.log(questionsID);
+  useEffect(() => {
+    // Getting the questions' ids if the D over 0
+    const questionsID = questionsData.map(item => valuesOverZero[0].filter(questions => questions.id === item.id));
+    console.log(questionsID);
 
-  const validQuestions = questionsID.filter(item => item.length > 0);
-  console.log(validQuestions);
+    // filtering questions when they're the same with questionsID
+    const validQuestions = questionsID.filter(item => item.length > 0);
+    const elements = validQuestions.map(item => item.map(element => element));
+    const element = elements.map((item) => item[0]);
+    console.log('element', element)
+    const filteredQuestions = element.map(atom => questionsData.filter(question => atom.id === question.id));
+    console.log(filteredQuestions);
+
+    const selectedQuestions = filteredQuestions.filter(item => item.length > 0);
+    console.log('selectedQuestions :>> ', selectedQuestions);
+  }, [valuesOverZero]);
+
   return (
     <div>
       {/* {questions.map((item) => (
